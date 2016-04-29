af9ac8229738731597f3e53c201e155d55acdb99
Sudan Vellakovil Kanaka Vel <sudan.kanakavel@maplebots.com>
2016-04-29 08:01:52 +0530

    Added files via upload

--- readingFile.js
+++ readingFile.js
@@ -1,3 +1,22 @@
+var Slack = require('node-slack');
+var slack = new Slack(hook_url,options);
+//var slack = new Slack(hook_url,{proxy: http_proxy});
+
+
+
+
+//Trying to send message to slack from Node.js app
+//I have used following package  https://github.com/xoxco/node-slack
+
+slack.send({
+    text: 'Meassage from node.js app to slack!',
+    channel: '#attendance',
+    username: 'Bot'
+});
+
+
+
+
   var fs = require("fs");
 //reading the file
  fs.readFile('Attendance01-04-2016.dat','utf8',function (err,data){
